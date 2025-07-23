48efbbded35e3b999062553c9a1e3153a12d30a3
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-07-24 01:10:48 +0300

    image pick

---
+++ Maximilian_Schwarzmuller-React-Nextjs_course/Coding-Exercise21/App.jsx
@@ -0,0 +1,55 @@
+// // Accessing DOM Elements with "refs"
+// // Your given a code snippet that's part of a bigger app that deals with user image uploads.
+
+// // Since the native, built-in <input type="file"> element is hard to style and doesn't fit the intended app style, it's hidden via display: none in the provided index.css file.
+
+// // Therefore, to make the file picker work without being displayed, your task is to ensure that the click event on the <input type="file"> element is triggered whenever the <button>Pick Image</button> is clicked.
+
+// // This can be achieved by calling the built-in click() method on the underlying input element.
+
+// // You should use React's "ref" feature to get hold of the <input type="file"> element and execute that click() method on it whenever the <button> is clicked.
+
+// // Important: In this Udemy exercise environment, React hooks must be used directly on the imported React object (import React from 'react'). For example, useState (which you don't need for this task) would then be called like this: React.useState().
+
+// // Access a hidden file picker element and open the file picker dialog when another button is pressed.
+
+// function App() {
+//     function handle(){
+        
+//     }
+//   return (
+//     <div id="app">
+//       <p>Please select an image</p>
+//       <p>
+//         <input data-testid="file-picker" ref={a} type="file" accept="image/*" />
+//         <button>Pick Image</button>
+//       </p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Answer:
+
+import React from 'react';
+
+function App() {
+    const fileInputRef = React.useRef();
+    
+    function handlePickImage(){
+        fileInputRef.current.click();
+    }
+    
+  return (
+    <div id="app">
+      <p>Please select an image</p>
+      <p>
+        <input data-testid="file-picker" ref={fileInputRef} type="file" accept="image/*" />
+        <button onClick={handlePickImage}>Pick Image</button>
+      </p>
+    </div>
+  );
+}
+
+export default App;
