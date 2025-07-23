dd2334d1fc35c19f79a38c90dd013f5d8985b474
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-07-23 23:48:36 +0300

    Implement dynamic styling for h1 element based on button clicks and add associated CSS styles

---
+++ Maximilian_Schwarzmuller-React-Nextjs_course/Coding-Exercise17/Coding-Exercise17.jsx
@@ -0,0 +1,40 @@
+
+// Dynamic Styling with Inline Styles
+// Your task is to help out a colleague and style a h1 element dynamically (with inline styles) depending on which button was pressed.
+
+// If the "Yes" button is pressed, the h1 element should receive color: "green" as an inline style. If the "No" button is pressed, color: "red" should be applied.
+
+// Initially, when no button has been clicked yet, the color should be set to "white".
+
+// Important: You must use these specific colors ("green", "red", "white") - don't use any hex code or slight variations of these colors!
+
+// In addition, this Udemy exercise workspace does not support the direct usage of React Hooks - instead, import React from 'react' and then use Hooks like this: React.useState().
+
+// Dynamically style a heading depending on which button is pressed.
+
+// Answer:
+
+import React from "react";
+function App() {
+    const [theColor , setTheColor] = React.useState("");
+    return (
+   <> <div id="app">
+      <h1 style={{ color:theColor === "yes" ?"green" : theColor === "no" ? "red" : "white" }}>CSS is great!</h1>
+      <menu>
+        <li>
+          <button onClick={
+              ()=>setTheColor("yes")
+          }>Yes</button>
+        </li>
+        <li>
+          <button onClick={
+              ()=>setTheColor("no")
+          }>No</button>
+        </li>
+      </menu>
+    </div>
+    </>
+  );
+}
+
+export default App;
